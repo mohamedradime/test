365089b0fce1085413db4d537aa71e1fcb092c04
Mohamed Radi <74203569+mwradi@users.noreply.github.com>
2024-05-12 20:32:44 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Rubik } from "next/font/google";
-import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import "./globals.css";
 
